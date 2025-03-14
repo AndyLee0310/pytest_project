109097cea1f3cddd99bbe40a8dfed3f749deacc8
AndyLee0310 <t108590001@ntut.org.tw>
2025-03-15 03:39:16 +0800

    d

--- module/path.py
+++
@@ -1,7 +0,0 @@
-# 放置路徑相關資料
-import os
-
-BASE = os.path.dirname(os.path.dirname(__file__))
-REPORT = os.path.join(BASE, 'report')
-ALLURE_RESULTS = os.path.join(REPORT, 'allure-results')
-ALLURE_REPORT = os.path.join(REPORT, 'allure-report')
